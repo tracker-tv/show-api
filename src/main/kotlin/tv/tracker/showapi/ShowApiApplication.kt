d973b28ed67221c2da8531ee537230d9faf9e250
Florent Sorel <florent.sorel@gmail.com>
2024-07-20 16:50:36 +0200

    initial commit

---
+++ src/main/kotlin/tv/tracker/showapi/ShowApiApplication.kt
@@ -0,0 +1,11 @@
+package tv.tracker.showapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShowApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShowApiApplication>(*args)
+}
---
+++ src/test/kotlin/tv/tracker/showapi/ShowApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package tv.tracker.showapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShowApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
